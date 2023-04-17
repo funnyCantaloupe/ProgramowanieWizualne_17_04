557f51a281fe6abfbb475f08ef0a745472fb69f7
Aleksandra Karwat <alekarwat@gmail.com>
2023-04-17 22:34:57 +0200

    move

--- Form1.cs
+++ Form1.cs
@@ -34,5 +34,33 @@ namespace ProgramowanieWizualne_17_04
             }
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                Bitmap rotatedBitmap = new Bitmap(bitmap.Height, bitmap.Width); // Tworzenie nowego obrazka o obróconych wymiarach
+                Graphics gfx = Graphics.FromImage(rotatedBitmap);
+                gfx.Clear(Color.White); // Wyczyść nowy obrazek tłem w kolorze białym
+                gfx.TranslateTransform((float)bitmap.Height / 2, (float)bitmap.Width / 2);
+                gfx.RotateTransform(90);
+                gfx.TranslateTransform(-(float)bitmap.Width / 2, -(float)bitmap.Height / 2);
+                gfx.DrawImage(bitmap, new Point(0, 0));
+                pictureBox1.Image = rotatedBitmap;
+                gfx.Dispose();
+                bitmap.Dispose();
+            
+
+        }
     }
 }
